461736bd68fbd6e0f8c4c18384876f5adc8aefb9
BeepBoopYeetYourScoot <zdmitry@alabuga.ru>
2020-12-07 10:20:40 +0300

    Removed the wrongly named directory

--- BasicPython/1.Variables/boolean.py
+++
@@ -1,8 +0,0 @@
-a = True
-b = False
-
-if a == 1:
-    print('opa bratok predmetik')
-
-if b == False:
-    print('opa nlb4ka')
--- BasicPython/5.ListsTuplesSets/empty.py
+++
@@ -1,9 +0,0 @@
-empty_list = []
-empty_list_1 = list()
-
-empty_tuple = ()
-empty_tuple_1 = tuple()
-
-empty_set = {}  # Это неправильно, потому что это будет пустой словарь
-empty_set_1 = set()
-
